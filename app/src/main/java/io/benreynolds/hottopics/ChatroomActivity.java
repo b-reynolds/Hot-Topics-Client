845f122089e7c2a15abd754a4b2e9e54e3bedfcc
Ben Reynolds <breynolds1994@outlook.com>
2018-01-08 17:37:42 +0000

    Implement basic chatting functionality

--- app/src/main/java/io/benreynolds/hottopics/ChatroomActivity.java
+++ app/src/main/java/io/benreynolds/hottopics/ChatroomActivity.java
@@ -2,12 +2,78 @@ package io.benreynolds.hottopics;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.benreynolds.hottopics.packets.ReceiveMessagePacket;
+import io.benreynolds.hottopics.packets.SendMessagePacket;
 
 public class ChatroomActivity extends AppCompatActivity {
+
+    private WebSocketCommunicator mWebSocketCommunicator = WebSocketCommunicator.getInstance();
+
+    final List<String> mMessages = new ArrayList<>();
+    private ArrayAdapter<String> mMessageListAdapter;
+
+    private Button mBtnSend;
+    private EditText mMessage;
+
+
+    private ListView mMessageList;
+    public class UpdateChatFeed implements Runnable {
+
+        @Override
+        public void run() {
+            while(!Thread.currentThread().isInterrupted()) {
+                ReceiveMessagePacket receiveMessagePacket = mWebSocketCommunicator.pollPacket(ReceiveMessagePacket.class);
+                if(receiveMessagePacket == null) {
+                    Thread.yield();
+                    continue;
+                }
+
+                mMessages.add(receiveMessagePacket.getAuthor() + ": " + receiveMessagePacket.getMessage());
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mMessageListAdapter.notifyDataSetChanged();
+                    }
+                });
+            }
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chatroom);
+
+        mMessageListAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mMessages);
+        mMessageList = findViewById(R.id.lstMessages);
+        mMessageList.setAdapter(mMessageListAdapter);
+
+        mMessage = findViewById(R.id.txtMessage);
+
+        mBtnSend = findViewById(R.id.btnSend);
+        mBtnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SendMessagePacket sendMessagePacket = new SendMessagePacket(mMessage.getText().toString());
+                mMessage.setText("");
+
+                mWebSocketCommunicator.sendPacket(sendMessagePacket);
+            }
+        });
+
+       new Thread(new UpdateChatFeed()).start();
+
+
     }
+
 }
--- app/src/main/java/io/benreynolds/hottopics/RoomListActivity.java
+++ app/src/main/java/io/benreynolds/hottopics/RoomListActivity.java
@@ -1,9 +1,13 @@
 package io.benreynolds.hottopics;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -13,6 +17,8 @@ import java.util.List;
 import io.benreynolds.hottopics.packets.Chatroom;
 import io.benreynolds.hottopics.packets.ChatroomsRequestPacket;
 import io.benreynolds.hottopics.packets.ChatroomsResponsePacket;
+import io.benreynolds.hottopics.packets.JoinChatroomRequestPacket;
+import io.benreynolds.hottopics.packets.JoinChatroomResponsePacket;
 
 public class RoomListActivity extends AppCompatActivity {
 
@@ -37,6 +43,18 @@ public class RoomListActivity extends AppCompatActivity {
         mRoomList.setAdapter(mRoomListAdapter);
 
         new Thread(new PopulateChatroomList()).start();
+
+        mRoomList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+                String selectedRoom = (String)mRoomList.getItemAtPosition(position);
+                Log.d(TAG, "Selected: " + selectedRoom);
+                new Thread(new JoinRoom(selectedRoom)).start();
+            }
+
+        });
+
     }
 
     public class PopulateChatroomList implements Runnable {
@@ -58,6 +76,34 @@ public class RoomListActivity extends AppCompatActivity {
             }
         }
 
+    }
+
+    public class JoinRoom implements Runnable {
+
+        private String mRoomName;
+
+        public JoinRoom(final String roomName) {
+            mRoomName = roomName;
+        }
+
+        @Override
+        public void run() {
+            mWebSocketCommunicator.sendPacket(new JoinChatroomRequestPacket(mRoomName));
+            while (!Thread.currentThread().isInterrupted()) {
+                JoinChatroomResponsePacket responsePacket = mWebSocketCommunicator.pollPacket(JoinChatroomResponsePacket.class);
+                if (responsePacket == null) {
+                    continue;
+                }
+
+                if(!responsePacket.getResponse()) {
+                    break;
+                }
+
+                Intent chatRoom = new Intent(RoomListActivity.this, ChatroomActivity.class);
+                chatRoom.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(chatRoom);
+            }
+        }
     }
 
 }
