310621f68720602ce0bcf5b7f9d0320027fb6434
Ben Reynolds <breynolds1994@outlook.com>
2018-04-10 03:31:10 +0100

    Add button on chatroom list that google searches trend

--- app/src/main/java/io/benreynolds/hottopics/ChatroomListAdapter.java
+++ app/src/main/java/io/benreynolds/hottopics/ChatroomListAdapter.java
@@ -1,43 +1,66 @@
 package io.benreynolds.hottopics;
 
+import android.app.SearchManager;
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 import io.benreynolds.hottopics.packets.Chatroom;
 
+import static android.support.v4.content.ContextCompat.startActivity;
+
 public class ChatroomListAdapter extends ArrayAdapter<Chatroom> {
+
+    private Context mContext;
 
     ChatroomListAdapter(Context context, ArrayList<Chatroom> chatrooms) {
         super(context, 0, chatrooms);
+        mContext = context;
     }
 
     @NonNull
     @Override
     public View getView(int position, View convertView, @NonNull ViewGroup parent) {
-        Chatroom chatroom = getItem(position);
+        final Chatroom chatroom = getItem(position);
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.chatroom_row, parent,
                     false);
         }
 
-        TextView roomName = convertView.findViewById(R.id.textView_roomName);
-        TextView roomSize = convertView.findViewById(R.id.textView_roomSize);
+        TextView tvChatroomName = convertView.findViewById(R.id.tvChatroomName);
+        TextView tvUsersInChatroom = convertView.findViewById(R.id.tvUsersInChatroom);
+        Button ibSearch = convertView.findViewById(R.id.ibSearch);
+        ibSearch.setFocusable(false);
+
+
 
         if(chatroom == null) {
-            roomName.setText(R.string.null_string);
-            roomSize.setText(R.string.null_string);
+            tvChatroomName.setText(R.string.null_string);
+            tvUsersInChatroom.setText(R.string.null_string);
         }
         else {
-            roomName.setText(chatroom.getName());
-            roomSize.setText(String.format("%s User(s)", chatroom.getSize()));
+            tvChatroomName.setText(chatroom.getName());
+            tvUsersInChatroom.setText(String.format("%s User(s)", chatroom.getSize()));
+            ibSearch.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+                    intent.putExtra(SearchManager.QUERY, chatroom.getName()); // query contains search string
+                    mContext.startActivity(intent);
+                }
+            });
         }
+
+
 
         return convertView;
     }
