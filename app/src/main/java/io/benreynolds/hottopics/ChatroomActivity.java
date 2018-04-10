63e240745ea66d168bbbe9e75360f4f7e9272cb6
Ben Reynolds <breynolds1994@outlook.com>
2018-04-10 02:15:36 +0100

    Refactor chat message row style and remove unused imports

--- app/src/main/java/io/benreynolds/hottopics/ChatroomActivity.java
+++ app/src/main/java/io/benreynolds/hottopics/ChatroomActivity.java
@@ -1,12 +1,10 @@
 package io.benreynolds.hottopics;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.text.Html;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ListView;
